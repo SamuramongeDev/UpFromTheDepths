aa657f544b9d1f68d81ee79c49527931eb2fdc7a
SamuramongeDev <giovandres1325@gmail.com>
2024-01-20 15:30:47 -0300

    Agregar archivos de proyecto.

---
+++ UpFromTheDepths/Plugin.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+
+namespace UpFromTheDepths
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("com.snmodding.nautilus")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public new static ManualLogSource Logger { get; private set; }
+
+        private static readonly Harmony harmony = new(PluginInfo.PLUGIN_GUID);
+
+        // Set up the mod.
+        private void Awake()
+        {
+            Logger = base.Logger;
+
+            // Run patches and initialize things.
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            // Not strictly necessary but is good to have.
+            Logger.LogInfo("UpFromTheDepths loaded correctly!");
+        }
+    }
+}
